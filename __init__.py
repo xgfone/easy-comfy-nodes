f322be9d61e22a01baa1d35610ccf130942ab46c
xgfone <xgfone@126.com>
2026-07-23 18:51:13 +0800

    format the code

--- __init__.py
+++ __init__.py
@@ -1,102 +1,133 @@
-import requests
 import base64
 import io
-import numpy as np
-from PIL import Image, ImageOps
-import torch
+
 import boto3
 import comfy
-import pillow_avif
-
+import numpy as np
+import pillow_avif as _
+import requests
+import torch
+from PIL import Image, ImageOps
 from pillow_heif import register_heif_opener
+
 register_heif_opener()
+
 
 class HttpPostNode:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {"url": ("STRING", {"default": ""}), "body": ("DICT",)}}
-    RETURN_TYPES = ("INT", )
-    RETURN_NAMES=("status_code",)
+
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("status_code",)
     FUNCTION = "execute"
     CATEGORY = "HTTP"
-    OUTPUT_NODE=True
+    OUTPUT_NODE = True
 
     def execute(self, url, body):
         response = requests.post(url, json=body)
         print(response, response.status_code, response.text)
         return (response.status_code,)
 
+
 class EmptyDictNode:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {}}
-    RETURN_TYPES = ("DICT", )
-    RETURN_NAMES=("dict",)
+
+    RETURN_TYPES = ("DICT",)
+    RETURN_NAMES = ("dict",)
     FUNCTION = "execute"
     CATEGORY = "DICT"
 
     def execute(self):
         return ({},)
 
+
 class AssocStrNode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"dict": ("DICT",), "key": ("STRING", {"default": ""}), "value": ("STRING", {"default": ""})}}
-    RETURN_TYPES = ("DICT", )
-    RETURN_NAMES=("dict",)
+        return {
+            "required": {
+                "dict": ("DICT",),
+                "key": ("STRING", {"default": ""}),
+                "value": ("STRING", {"default": ""}),
+            }
+        }
+
+    RETURN_TYPES = ("DICT",)
+    RETURN_NAMES = ("dict",)
     FUNCTION = "execute"
     CATEGORY = "DICT"
 
     def execute(self, dict, key, value):
         return ({**dict, key: value},)
+
 
 class AssocDictNode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"dict": ("DICT",), "key": ("STRING", {"default": ""}), "value": ("DICT", {"default": {}})}}
-    RETURN_TYPES = ("DICT", )
-    RETURN_NAMES=("dict",)
+        return {
+            "required": {
+                "dict": ("DICT",),
+                "key": ("STRING", {"default": ""}),
+                "value": ("DICT", {"default": {}}),
+            }
+        }
+
+    RETURN_TYPES = ("DICT",)
+    RETURN_NAMES = ("dict",)
     FUNCTION = "execute"
     CATEGORY = "DICT"
 
     def execute(self, dict, key, value):
         return ({**dict, key: value},)
+
 
 class AssocImgNode:
     @classmethod
     def INPUT_TYPES(s):
         return {
-                   "required": {
-                       "dict": ("DICT",),
-                       "key": ("STRING", {"default": ""}),
-                       "value": ("IMAGE", {"default": ""}),
-                   },
-                   "optional": {
-                       "format": ("STRING", {"default": "webp"}),
-                       "quality": ("INT", {"default": 92})
-                   }
-               }
-    RETURN_TYPES = ("DICT", )
-    RETURN_NAMES=("dict",)
+            "required": {
+                "dict": ("DICT",),
+                "key": ("STRING", {"default": ""}),
+                "value": ("IMAGE", {"default": ""}),
+            },
+            "optional": {
+                "format": ("STRING", {"default": "webp"}),
+                "quality": ("INT", {"default": 92}),
+            },
+        }
+
+    RETURN_TYPES = ("DICT",)
+    RETURN_NAMES = ("dict",)
     FUNCTION = "execute"
     CATEGORY = "DICT"
 
     def execute(self, dict, key, value, format="webp", quality=92):
-        image = Image.fromarray(np.clip(255. * value[0].cpu().numpy(), 0, 255).astype(np.uint8))
+        image = Image.fromarray(np.clip(255.0 * value[0].cpu().numpy(), 0, 255).astype(np.uint8))
         buffered = io.BytesIO()
         image.save(buffered, format=format, quality=quality)
-        img_bytestr =  base64.b64encode(buffered.getvalue())
-        return ({**dict, key: (bytes(f'data:image/{format};base64,', encoding='utf-8') + img_bytestr).decode() },)
+        img_bytestr = base64.b64encode(buffered.getvalue())
+        return (
+            {
+                **dict,
+                key: (
+                    bytes(f"data:image/{format};base64,", encoding="utf-8") + img_bytestr
+                ).decode(),
+            },
+        )
+
 
 def loadImageFromUrl(url):
     # Lifted mostly from https://github.com/sipherxyz/comfyui-art-venture/blob/main/modules/nodes.py#L43
     if url.startswith("data:image/"):
         i = Image.open(io.BytesIO(base64.b64decode(url.split(",")[1])))
     elif url.startswith("s3://"):
-        s3 = boto3.client('s3')
+        s3 = boto3.client("s3")
         bucket, key = url.split("s3://")[1].split("/", 1)
         obj = s3.get_object(Bucket=bucket, Key=key)
-        i = Image.open(io.BytesIO(obj['Body'].read()))
+        i = Image.open(io.BytesIO(obj["Body"].read()))
     else:
         response = requests.get(url, timeout=5)
         if response.status_code != 200:
@@ -124,24 +155,32 @@ def loadImageFromUrl(url):
 
     return (image, mask)
 
+
 class LoadImageFromUrlNode:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {"url": ("STRING", {"default": ""})}}
+
     RETURN_TYPES = ("IMAGE", "MASK")
-    RETURN_NAMES=("image", "mask")
+    RETURN_NAMES = ("image", "mask")
     FUNCTION = "execute"
     CATEGORY = "HTTP"
 
     def execute(self, url):
         return {"result": loadImageFromUrl(url)}
 
+
 class LoadImagesFromUrlsNode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"urls": ("STRING", {"default": "", "multiline": True, "dynamicPrompts": False})}}
+        return {
+            "required": {
+                "urls": ("STRING", {"default": "", "multiline": True, "dynamicPrompts": False})
+            }
+        }
+
     RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES=("images",)
+    RETURN_NAMES = ("images",)
     FUNCTION = "execute"
     CATEGORY = "HTTP"
 
@@ -156,9 +195,16 @@ class LoadImagesFromUrlsNode:
             image1 = firstImage
             for image2 in restImages:
                 if image1.shape[1:] != image2.shape[1:]:
-                    image2 = comfy.utils.common_upscale(image2.movedim(-1, 1), image1.shape[2], image1.shape[1], "bilinear", "center").movedim(1, -1)
+                    image2 = comfy.utils.common_upscale(
+                        image2.movedim(-1, 1),
+                        image1.shape[2],
+                        image1.shape[1],
+                        "bilinear",
+                        "center",
+                    ).movedim(1, -1)
                 image1 = torch.cat((image1, image2), dim=0)
             return (image1,)
+
 
 class S3Upload:
     """
@@ -189,11 +235,12 @@ class S3Upload:
         s3_bucket="",
         s3_object_name="",
     ):
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource("s3")
         s3.Bucket(s3_bucket).upload_file(filenames[1][1], s3_object_name)
-        s3url = f's3://{s3_bucket}/{s3_object_name}'
-        print(f'Uploading file to {s3url}')
+        s3url = f"s3://{s3_bucket}/{s3_object_name}"
+        print(f"Uploading file to {s3url}")
         return (s3url,)
+
 
 NODE_CLASS_MAPPINGS = {
     "EZHttpPostNode": HttpPostNode,
